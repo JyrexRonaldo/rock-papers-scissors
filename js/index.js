c3dfbd574c9619bdcc12f7e7c6fab737208d1e12
Jyrex Ronaldo <jyrexgt38@gmail.com>
2022-03-20 00:43:25 +0000

    Introduce a GUI for rock paper scissors

--- js/index.js
+++ js/index.js
@@ -1,3 +1,54 @@
+const buttons = document.querySelectorAll('button');
+const resultDiv = document.querySelector('div');
+let round = 0;
+let playerScore = 0;
+let computerScore = 0;
+
+for (i = 0; i < buttons.length; i++) {
+    buttons[i].addEventListener('click', (e) => {
+        let result = playRound(e.target.textContent, computerPlay());
+        if (playerScore === 5 && computerScore === 5) {
+            resultDiv.innerHTML += `<br>You tied! play again!`;
+            reset();
+        } else if (playerScore === 5) {
+            resultDiv.innerHTML += `<br>Yay, you win!`;
+            reset();
+        } else if (computerScore === 5) {
+            resultDiv.innerHTML += `<br>You lose! better luck next time`;
+            reset();
+        } else {
+            if (result.includes('Win')) {
+                playerScore += 1;
+            } else if (result.includes('Lose')) {
+                computerScore += 1;
+            } else {
+                playerScore += 1;
+                computerScore += 1;
+            }
+            
+            resultDiv.innerHTML = `Result: ${result} 
+            <br>Round: ${round}
+            <br>Player score: ${playerScore}
+            <br>Computer score: ${computerScore}`;
+
+        }
+
+        round += 1;
+
+        
+    });
+    
+}
+
+function reset() {
+    round = 0;
+    playerScore = 0;
+    computerScore = 0;
+    resultDiv.innerHTML += ``;
+}
+
+
+
 function computerPlay() {
     let choiceNumber = Math.floor(Math.random() * 3);
     if (choiceNumber === 0) {
@@ -12,7 +63,8 @@ function computerPlay() {
 
 function playRound(playerSelection, computerSelection) {
 
-    playerSelection = (playerSelection.slice(0, 1)).toUpperCase() + (playerSelection.substr(1, (playerSelection.length - 1))).toLowerCase();
+    playerSelection = (playerSelection.slice(0, 1)).toUpperCase() + 
+    (playerSelection.substr(1, (playerSelection.length - 1))).toLowerCase();
 
     if (computerSelection === 'Rock') {
         if (playerSelection === 'Rock') {
@@ -41,33 +93,33 @@ function playRound(playerSelection, computerSelection) {
     }
 }
 
-function game() {
-    let round = 0;
-    let playerScore = 0;
-    let computerScore = 0;
+// function game() {
+//     let round = 0;
+//     let playerScore = 0;
+//     let computerScore = 0;
 
-    for (i = 0; i < 5; i++) {
-        let choice = prompt('Rock, paper or scissors?'); 
-        let result = playRound(choice, computerPlay())
+//     for (i = 0; i < 5; i++) {
+//         let choice = prompt('Rock, paper or scissors?'); 
+//         let result = playRound(choice, computerPlay())
 
-        if (result.includes('Win')) {
-            playerScore += 1;
-        } else if (result.includes('Lose')) {
-            computerScore += 1;
-        } else {
-            playerScore += 1;
-            computerScore += 1;
-        }
-        round += 1;
-    }
+//         if (result.includes('Win')) {
+//             playerScore += 1;
+//         } else if (result.includes('Lose')) {
+//             computerScore += 1;
+//         } else {
+//             playerScore += 1;
+//             computerScore += 1;
+//         }
+//         round += 1;
+//     }
 
-    if (playerScore > computerScore) {
-        console.log('Yay, you win!');
-    } else if (playerScore < computerScore) {
-        console.log('You lose! better luck next time')
-    } else {
-        console.log('You tied! play again!')
-    }
-}
+//     if (playerScore > computerScore) {
+//         console.log('Yay, you win!');
+//     } else if (playerScore < computerScore) {
+//         console.log('You lose! better luck next time')
+//     } else {
+//         console.log('You tied! play again!')
+//     }
+// }
 
-game();
+// game();
